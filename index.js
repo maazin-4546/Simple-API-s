fa6652651dc69b630942aa446110627b64f4c146
maazin-4546 <maazin.noorisys@gmail.com>
2025-04-17 20:48:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/about", (req, res) => {
     res.send("about page")
 })
 
+app.get("/service", (req, res) => {
+    res.send("service page")
+})
+
 app.listen(8000, () => {
     console.log("server is running")
 })
